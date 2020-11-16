40a8d033c196880e38598303dd656a98001e0872
jaeyeon23 <catblues282@gmail.com>
2020-11-17 00:01:21 +0900

    Initial commit

---
+++ src/main/java/me/jaeyeon23/restapibyspring/RestApiBySpringApplication.java
@@ -0,0 +1,13 @@
+package me.jaeyeon23.restapibyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiBySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiBySpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/jaeyeon23/restapibyspring/RestApiBySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jaeyeon23.restapibyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiBySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
